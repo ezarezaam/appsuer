0f5716454bce87e55864e94be7f221c6fd0b3f1d
ezarezaam <eza.rezaam@gmail.com>
2025-11-03 05:32:11 +0700

    Move functions to root directory and simplify configuration

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,17 +1,32 @@
+// Simple Netlify function
 exports.handler = async (event, context) => {
+  console.log('Function called:', event.httpMethod, event.path);
+  
+  // Handle CORS preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+      'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
       message: 'Hello from Netlify Functions!',
       timestamp: new Date().toISOString(),
       method: event.httpMethod,
-      path: event.path
+      path: event.path,
+      headers: event.headers
     })
   };
 };
